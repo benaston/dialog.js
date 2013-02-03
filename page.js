163e8bd498fd9ecc0fca2269b203ebdb286bfd99
Ben Aston <ben.aston@rbi.co.uk>
2013-02-03 17:57:14 +0000

    Button styling update.

--- page.js
+++ page.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function () {
     new app.DialogView(new app.DialogModel());
+	
+	//used to unify font line height appearance Mac/PC
+	if (navigator.userAgent.indexOf('Mac') > 0) {
+		$("body").addClass("mac");
+	}
 }); 
